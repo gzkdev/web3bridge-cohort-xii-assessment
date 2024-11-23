30acd53ecf4818c651fb5dd871131f7d501a1ccd
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-11-23 12:51:49 +0100

    add localstorage support in useStudents hook

--- src/hooks/useStudents.ts
+++ src/hooks/useStudents.ts
@@ -1,8 +1,8 @@
-import { useState } from "react";
 import { Student } from "../lib/types";
+import { useLocalStorage } from "usehooks-ts";
 
 export const useStudents = () => {
-  const [students, setStudents] = useState<Student[]>([]);
+  const [students, setStudents] = useLocalStorage<Student[]>("students", []);
 
   const handleRegister = (student: Student) => {
     setStudents([...students, student]);
